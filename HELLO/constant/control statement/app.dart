f1064026b298e0591491f8ce8c56587dff07046f
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:13:51 +0200

    dart

--- HELLO/constant/control statement/app.dart
+++ HELLO/constant/control statement/app.dart
@@ -17,23 +17,16 @@ var marks=70;
 if(marks>=90 && marks<100){
 print("A + grade");
 }
-else if(marks>= 80 && marks<90){
-
-print("B grade");
-}
-else if(marks >= 70 && marks<0){
-
-print("C grade");
-}
-else if(marks >= 80 && marks<90){
-
-print("D grade");
-}
-else if(marks >= 80 && marks<90){
-
-print("E grade");
-}
-else{
-  print('You have failed');
-}
-}
+ else if (marks >= 80 && marks < 90) {
+		print("A grade");
+	} else if (marks >= 70 && marks < 80) {
+		print("B grade");
+	} else if (marks >= 60 && marks < 70) {
+		print("C grade");
+	} else if (marks > 30 && marks < 60) {
+		print("D grade");
+	} else if (marks >= 0 && marks < 30) {
+		print("You have failed");
+	} else {
+		print("Invalid Marks. Please try again !");
+	}}
