b0bf9e4c75e8d7f8bf5a78d2f5fd6d0cce39641f
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:01:01 +0200

    dart

---
+++ HELLO/constant/control statement/app.dart
@@ -0,0 +1,11 @@
+void main(){
+  var salary=2230;
+  if(salary > 123){
+    print("you have got promotion for this occur")  ;
+    }
+  
+    else{
+      print("you go need to work more harder");
+    }
+  }
+}
