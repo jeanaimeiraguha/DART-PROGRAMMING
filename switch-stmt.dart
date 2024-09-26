390181ccbef984e4df97053adbe04896551b1266
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:55:37 +0200

    dart

---
+++ switch-stmt.dart
@@ -0,0 +1,21 @@
+void main(){
+  String grade = "A";
+  switch(grade){
+    case 'A':
+    print("excellent with Grade A");
+    break;
+    case 'B':
+    print("Very Good !");
+    break;
+    case 'C':
+    print("Very Good");
+    break;
+    case 'D':
+    print("Very good !");
+    case 'F':
+    print("You have failed");
+ break;
+ default:
+ print("Invalid Grade");
+  }
+}
