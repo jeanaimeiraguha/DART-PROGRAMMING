c31c7adbd006f2bda3afc298a62f0e0da7f4c8e6
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:08:17 +0200

    dart

--- HELLO/constant/control statement/app.dart
+++ HELLO/constant/control statement/app.dart
@@ -17,3 +17,19 @@ var marks=70;
 if(marks>=90 && marks<100){
 print("A + grade");
 }}
+else if(marks >= 80 && marks<90){
+
+print("B grade");
+}
+else if(marks >= 80 && marks<90){
+
+print("B grade");
+}
+else if(marks >= 80 && marks<90){
+
+print("B grade");
+}
+else if(marks >= 80 && marks<90){
+
+print("B grade");
+}
