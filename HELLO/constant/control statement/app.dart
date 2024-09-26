df75dd05a1c255bd7491ebcee1e1e483cdf7aced
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:06:12 +0200

    dart

--- HELLO/constant/control statement/app.dart
+++ HELLO/constant/control statement/app.dart
@@ -1,3 +1,5 @@
+/*
+
 void main(){
   var salary=2230;
   if(salary > 123){
@@ -7,4 +9,11 @@ void main(){
     else{
       print("you go need to work more harder");
     }
-  }
+  }*/
+
+  // USe of if else
+void main(){
+var marks=70;
+if(marks>=90 && marks<100){
+print("A + grade");
+}}
