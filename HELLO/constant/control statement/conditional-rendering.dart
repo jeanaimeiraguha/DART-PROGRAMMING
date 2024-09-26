509f654082dd18da2d235bd44e6ee773dd005f24
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 19:26:34 +0200

    dart

--- HELLO/constant/control statement/conditional-rendering.dart
+++ HELLO/constant/control statement/conditional-rendering.dart
@@ -1,6 +1,16 @@
+
+
+/*
 void main(){
     int num1=4;
     int num2=654;
     int smallNumber= num1<num2?num2 :num1;
     print("$smallNumber is smaler");
+}
+*/
+//go using null
+void main(){
+String name=null;
+String nameToprint=name ?? "Guest";
+print("nameToprint");
 }
