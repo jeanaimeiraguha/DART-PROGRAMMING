8fc18a1dde50569b181efe2054d8f5392568d0d4
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-26 18:57:27 +0200

    dart

---
+++ HELLO/constant/app.dart
@@ -0,0 +1,12 @@
+void main(){
+  //finally
+  final cityName = "Kigali";
+  final String CountryName="Rwanda";
+  print(cityName);
+  print(CountryName);
+  // use of const
+  const PI =3.14;
+  const double gravity =9.8;
+  print(gravity);
+
+}
