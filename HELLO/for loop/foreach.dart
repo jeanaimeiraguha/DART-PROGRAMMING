1162984ee9221214653b49dd9e18b687f04eb697
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2024-10-31 01:47:24 -0700

    Foreach

---
+++ HELLO/for loop/foreach.dart
@@ -0,0 +1,7 @@
+void main() {
+  //Use string as datatype
+  print("Use string as datatype");
+  Set<String> names = {"Iraguha", "Mugisha","Mucyo"};
+  names.forEach((names) {
+    print(names);
+  });
