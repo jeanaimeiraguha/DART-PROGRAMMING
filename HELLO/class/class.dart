b1a97212ccb1ad6a4f7795d141613b4d89beccf4
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2024-11-07 00:46:24 -0800

    dart

--- HELLO/class/class.dart
+++ HELLO/class/class.dart
@@ -3,7 +3,7 @@ class Vehicles{
    var name;
   var category;
   void display(){
-    print("This is $name with in $category with color of $color");
+    print("This is $name with is used $category with color of $color");
   }
 }
 class Volvo extends Vehicles{
@@ -13,8 +13,10 @@ void Hoho(){
 }
 void main(){
   var D= new Vehicles();
-  D.display();
+
   D.name="Tax";
   D.color="Red";
+  D.category="Driving";
+    D.display();
 
 }
