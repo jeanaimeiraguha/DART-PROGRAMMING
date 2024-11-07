f291c8395eb76728e0f3596744b8696ad69d922c
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2024-11-07 01:10:07 -0800

    dart

--- HELLO/class/class.dart
+++ HELLO/class/class.dart
@@ -7,9 +7,25 @@ class Vehicles{
   }
 }
 class Volvo extends Vehicles{
+
 void Hoho(){
   print("This $name has also $color");
 }
+}
+class Drivers{
+  var name;
+  var address;
+  void info(){
+    print("He is called $name who lives $address");
+  }
+}
+class Salary extends Drivers{
+  void  main(){
+    var uhm =new Drivers();
+    uhm.name="Cyusa";
+    uhm.address="Nyamata";
+    uhm.info();
+  }
 }
 void main(){
   var D= new Vehicles();
